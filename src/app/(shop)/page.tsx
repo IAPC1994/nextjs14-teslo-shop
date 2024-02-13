d4e73bfba6c26bacb3faae3bb6e87c2879a37138
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-02-12 21:45:18 -0300

    feat(web): Routes in application (shop and auth)

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,9 +2,9 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <h1 className={ `${ titleFont.className }`}>Hello World</h1>
       <h1 className={ `${ titleFont.className } font-bold`}>Hello World</h1>
-    </main>
+    </div>
   );
 }

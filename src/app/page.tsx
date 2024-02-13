e8a8882e7df6228eb0a58cc9f655536a5ddd429c
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-02-12 21:16:04 -0300

    feat(web): fonts config file created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Hello World</h1>
+      <h1 className={ `${ titleFont.className }`}>Hello World</h1>
+      <h1 className={ `${ titleFont.className } font-bold`}>Hello World</h1>
     </main>
   );
 }

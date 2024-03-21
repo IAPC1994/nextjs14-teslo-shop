2212df4310ae895e704843d360c5349687cbc7f6
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-03-20 22:50:14 -0300

    ui(web): Product page initial design

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
+export * from "./product/quantity-selector/QuantitySelector";
+export * from "./product/size-selector/SizeSelector";
 export * from "./ui/sidebar/Sidebar";
 export * from "./products/product-grid/ProductGridItem";
 export * from "./products/product-grid/ProductGrid";
 export * from "./ui/title/Title";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/top-menu/TopMenu";
-
